f413e776d0169104fbf11a9223d6e597a42c89c7
TD-lipan <pan.li@china.talkdesk.com>
2022-06-17 21:30:33 +0800

    feat: add input box and filter bar

---
+++ src/components/filter-bar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.less';
+
+export default function () {
+    return (
+        <div className={styles.filterBarDefault}>
+           <div></div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import styles from './index.less';
 import Background from '../components/background';
 import NavigationBar from '../components/navigation-bar';
+import FilterBar from '../components/filter-bar';
+import InputBox from '../components/input-box';
 
 export default function IndexPage() {
   return (
     <div>
-      <div className={styles.scene}><NavigationBar /></div>
+      <NavigationBar />
+      <FilterBar />
+      <InputBox />
       <Background />
     </div>
   );
