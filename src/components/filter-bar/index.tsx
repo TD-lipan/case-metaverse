e382e2164014a999eebc1fd82fbc094338e32496
TD-lipan <pan.li@china.talkdesk.com>
2022-06-17 22:00:49 +0800

    feat: multi tab

--- src/components/filter-bar/index.tsx
+++ src/components/filter-bar/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './index.less';
 
-export default function () {
+export default function ({ type }: { type?: 'default' | 'hover' | 'selected' }) {
+    const filterBarClass = useMemo(() => {
+        switch (type) {
+            case 'hover':
+                return styles.filterBarHover;
+            case 'selected':
+                return styles.filterBarSelected;
+            case 'default':
+            default:
+                return styles.filterBarDefault;
+        }
+    }, [])
     return (
-        <div className={styles.filterBarDefault}>
-           <div></div>
+        <div className={filterBarClass}>
+            <div></div>
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import Background from '../components/background';
 import NavigationBar from '../components/navigation-bar';
 import FilterBar from '../components/filter-bar';
 import InputBox from '../components/input-box';
+import MultiTab from '../components/multi-tab';
 
 export default function IndexPage() {
   return (
     <div>
       <NavigationBar />
-      <FilterBar />
+      <FilterBar type='default' />
       <InputBox />
+      <MultiTab index={2} />
       <Background />
     </div>
   );
