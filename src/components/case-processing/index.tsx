e1ba0c300cae4ebeb38259d31d5ab9b0f216b884
TD-lipan <pan.li@china.talkdesk.com>
2022-06-18 19:27:08 +0800

    feat: automatic typing

--- src/components/case-processing/index.tsx
+++ src/components/case-processing/index.tsx
@@ -5,20 +5,55 @@ import ChartArea from './ChartArea';
 import useDrag from './use-drag';
 
 import orderDetailsDrag from './images/order_details_drag.png';
+import { useEffect } from 'react';
+import AutomaticTyping from './AutomaticTyping';
+
+const wordsMap: { [index: number]: string } = {
+    0: "Could you see the VR model I sent you?",
+    2: "You could choose any font, color, and content you like and engrave it on the product. I will recommend you choose the center of the box. What do you want to write for your niece?",
+    6: "I could try to add a cute sticker on it. Do you like this?",
+    8: "No problem"
+};
 
 export default function () {
     const { noDarg, draging, dragX, dragY, targetAreaHover, onEndMouseOver, onEndMouseLeave, isDraged, setIsDraged, onStartDarg } = useDrag();
     const [showEmail, setShowEmail] = useState<boolean>(false);
+    const [step, setStep] = useState<number>(0);
+    const [words, setWords] = useState<string>("");
 
     const hanldeClick = () => {
-        setShowEmail(true);
-        setIsDraged(false);
+        if (step === 0 || step === 2 || step === 6 || step === 8) {
+            setStep(step + 1);
+            autoShow();
+            setWords("");
+        }
+        if (step >= 12) {
+            setShowEmail(true);
+            setIsDraged(false);
+        }
     }
+
+    const autoShow = async () => {
+        if (step === 0 || step === 2 || step === 6 || step === 8 || step >= 13) {
+            setWords(wordsMap[step]);
+        } else {
+            await new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(null);
+                }, 1000);
+            })
+            setStep(prev => prev + 1);
+        }
+    }
+
+    useEffect(() => {
+        autoShow();
+    }, [step])
 
     return (
         <>
             <div className={styles.workspace} />
-            <ChartArea onStartDarg={onStartDarg} showEamil={showEmail} />
+            <ChartArea onStartDarg={onStartDarg} showEamil={showEmail} step={step} />
             {/* <img className={styles.callDarg} src={callDrag} onDragStart={noDarg} style={{ display: draging ? 'block' : 'none', left: dragX, top: dragY }} /> */}
             <img className={styles.orderDetailsDrag} src={orderDetailsDrag} onDragStart={noDarg} style={{ display: draging ? 'block' : 'none', left: dragX, top: dragY }} />
             <div className={`${styles.targetArea} ${targetAreaHover}`} onMouseOver={onEndMouseOver} onMouseLeave={onEndMouseLeave}></div>
@@ -26,7 +61,7 @@ export default function () {
             {isDraged && <div className={styles.orderPaidInput} onClick={() => setIsDraged(false)} ></div>}
 
             <ChannelBar />
-
+            <AutomaticTyping str={words} />
             <div className={styles.typeSomething}>Type something...</div>
             <div className={styles.toolbar} />
             <div className={styles.customerToolbar} />
