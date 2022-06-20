ada6f4d41138291b3faf428433279bdf903c438f
TD-lipan <pan.li@china.talkdesk.com>
2022-06-20 19:45:01 +0800

    feat:  update components

--- src/components/customer-profile/index.tsx
+++ src/components/customer-profile/index.tsx
@@ -7,11 +7,10 @@ import profile2_1 from './images/profile2_0.png';
 import profile3_1 from './images/profile3_0.png';
 import profile4_1 from './images/profile4_0.png';
 
-export default function ({ isDraged }: { isDraged: boolean }) {
+export default function ({ left = 650, top = 244, visible = true, isDraged }: { left: number, top: number, visible: boolean, isDraged: boolean }) {
     const [src, setSrc] = useState<string>(profile1_1);
-
     return (
-        <div className={styles.customerProfile}>
+        <div className={styles.customerProfile} style={{ top: top + 'px', left: left + 'px', display: visible ? 'block' : 'none' }}>
             <div className={styles.tabs}>
                 <div className={styles.div1} onClick={() => { setSrc(profile1_1) }}></div>
                 <div className={styles.div2} onClick={() => { setSrc(profile2_1) }}></div>
